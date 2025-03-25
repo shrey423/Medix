834130c5486c3a5f125171d032a70cc1d7dfad9b
Shrey raj <120704020+shrey423@users.noreply.github.com>
2025-03-25 17:59:11 +0530

    python commit

--- frontend/src/components/consultation/services/api.ts
+++ frontend/src/components/consultation/services/api.ts
@@ -1,5 +1,5 @@
-const BACKEND_URL = "http://localhost:3001";
-const ML_URL = "http://127.0.0.1:8000";//AWS
+const BACKEND_URL = "https://medix-g0nk.onrender.com/";
+const ML_URL = "https://medixpython.onrender.com/";//AWS
 // const BACKEND_URL = "https://swasthya-sampark.onrender.com";
 // const BACKEND_URL = "https://connectihealth-backend.vercel.app"; // production1 state-less
 // const BACKEND_URL = "https://connect-health.onrender.com"; // production2 state-full
